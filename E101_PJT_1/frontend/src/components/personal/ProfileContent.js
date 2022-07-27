98a4e972e9e17b75f151b4acf8898a1349cf9cf4
DESKTOP-KVCQHCD\SSAFY <kjasm0714@naver.com>
2022-07-27 17:50:45 +0900

    feat : 회원정보 컴포넌트 추가
    body : 회원정보 컴포넌트 추가 및 백그라운드 이미지 추가
    close : 김혜라

--- E101_PJT_1/frontend/src/components/personal/ProfileContent.js
+++ E101_PJT_1/frontend/src/components/personal/ProfileContent.js
@@ -6,12 +6,67 @@ const ProfileContentBlock = styled.div`
   width: 100%;
   margin: 10px;
   padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column; 
+`;
+
+/**
+ * 스타일링된 input
+ */
+ const StyledInput = styled.input`
+ background: #E2D6BA;
+ border: 3px solid #000000;
+ border-radius: 5px;
+ font-size: 1.2rem;
+ padding: 1rem 0.5rem;
+ width: 100%;    
+ height: 100%;
+ ::placeholder {
+   font-size: 1.2rem;    
+ }
+ &:focus {
+   border: 3px solid white;
+   ::placeholder {
+     color: transparent;
+   }
+   /* border-bottom: 1px solid yellow; */
+ }
+ & + & {
+   margin-top: 1rem;
+ }
 `;
 
 const ProfileContent = () => {
   return (
     <ProfileContentBlock>
-      <h2>프로필 페이지입니다.</h2>
+      <h1>회원 정보</h1>
+      <StyledInput
+        autoComplete="userId"
+        name="userId"
+        placeholder="아이디"
+        // onChange={onChange}
+        // value={form.userId}
+      />
+
+      <StyledInput
+        autoComplete="userId"
+        name="userId"
+        placeholder="아이디"
+        // onChange={onChange}
+        // value={form.userId}
+      />
+
+      <StyledInput
+        autoComplete="userId"
+        name="userId"
+        placeholder="아이디"
+        // onChange={onChange}
+        // value={form.userId}
+      />
+
     </ProfileContentBlock>
   );
 };
