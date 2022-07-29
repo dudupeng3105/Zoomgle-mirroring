0b86d9db1d5010ea9381408bc0bde3fceeb98ef8
황인빈 <hibeen1@naver.com>
2022-07-29 10:37:40 +0900

    style : user 컨트롤러 url 변경
    /users -> /api/users 로 변경
    
    220729 아침 회의 결과 반영

--- E101_PJT_1/backend/src/main/java/com/ssafy/api/controller/UserController.java
+++ E101_PJT_1/backend/src/main/java/com/ssafy/api/controller/UserController.java
@@ -4,19 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import com.ssafy.api.request.UserLoginPostReq;
 import com.ssafy.api.request.UserRegisterPostReq;
-import com.ssafy.api.response.UserLoginPostRes;
 import com.ssafy.api.response.UserRes;
 import com.ssafy.api.service.UserService;
 import com.ssafy.common.auth.SsafyUserDetails;
-import com.ssafy.common.model.response.BaseResponseBody;
-import com.ssafy.common.util.JwtTokenUtil;
 import com.ssafy.db.entity.User;
-import com.ssafy.db.repository.UserRepositorySupport;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -27,14 +21,12 @@ import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
-import static com.ssafy.db.entity.QUser.user;
-
 /**
  * 유저 관련 API 요청 처리를 위한 컨트롤러 정의.
  */
 @Api(value = "유저 API", tags = {"User"})
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 @CrossOrigin
 public class UserController {
 	
