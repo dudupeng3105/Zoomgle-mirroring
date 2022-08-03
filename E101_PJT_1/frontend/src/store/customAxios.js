fdd1c4eecf06f197ec7341eb25febc353b6ba187
DESKTOP-KVCQHCD\SSAFY <kjasm0714@naver.com>
2022-08-03 11:45:47 +0900

    feat : 주사위 기능 수정 및 스타일 추가
    주사위 1에서 3까지만 나오게 수정

--- E101_PJT_1/frontend/src/store/customAxios.js
+++ E101_PJT_1/frontend/src/store/customAxios.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 export const customAxios = axios.create({
-  // baseURL: 'https://i7e101.p.ssafy.io/',
-  baseURL: 'http://localhost:8080/',
+  baseURL: 'https://i7e101.p.ssafy.io/',
+  // baseURL: 'http://localhost:8080/',
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',    
   },
 });
 
 export const customJWTAxios = axios.create({
-  // baseURL: 'https://i7e101.p.ssafy.io/',
-  baseURL: 'http://localhost:8080/',
+  baseURL: 'https://i7e101.p.ssafy.io/',
+  // baseURL: 'http://localhost:8080/',
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',
     'Authorization': `Bearer ${localStorage.getItem('token')}`
