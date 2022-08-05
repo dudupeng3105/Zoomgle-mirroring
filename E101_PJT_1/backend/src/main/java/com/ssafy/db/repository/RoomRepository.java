fb5992cb39d6064094ccae457049997289cc2abf
chochanhong <choch953@gmail.com>
2022-08-05 15:38:03 +0900

    fix : 게임 초대 중복 관리
    게임 초대시 중복 체크 후 초대장 생성
    
    Close 조찬홍

--- E101_PJT_1/backend/src/main/java/com/ssafy/db/repository/RoomRepository.java
+++ E101_PJT_1/backend/src/main/java/com/ssafy/db/repository/RoomRepository.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
     // 아래와 같이, Query Method 인터페이스(반환값, 메소드명, 인자) 정의를 하면 자동으로 Query Method 구현됨.
-    Room findByHostAndDate(String host, String date);
 
     Optional<Room> findByRoomSeq(long roomSeq);
 }
