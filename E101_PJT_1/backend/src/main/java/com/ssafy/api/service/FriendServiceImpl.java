bd5ab5ad70f787c113e10e01dc779f7c1c233734
민경대 <rudwl1005@naver.com>
2022-08-13 01:00:44 +0900

    feat : 친구삭제 API 추가
    친구 삭제 기능 추가

--- E101_PJT_1/backend/src/main/java/com/ssafy/api/service/FriendServiceImpl.java
+++ E101_PJT_1/backend/src/main/java/com/ssafy/api/service/FriendServiceImpl.java
@@ -5,6 +5,7 @@ import com.ssafy.db.entity.Friend;
 import com.ssafy.db.repository.FriendRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,41 +14,59 @@ import java.util.List;
  */
 @Service("friendService")
 public class FriendServiceImpl implements FriendService {
-	@Autowired
-	FriendRepository friendRepository;
+    @Autowired
+    FriendRepository friendRepository;
 
-	/**
-	 * 내 닉네임으로 친구 정보 얻어오는 메서드
-	 */
-	@Override
-	public List<FriendInfoInterface> getAllFriendInfo(String myNickname) {
-		return friendRepository.findAllFriendByNickname(myNickname);
-	}
+    /**
+     * 내 닉네임으로 친구 정보 얻어오는 메서드
+     */
+    @Override
+    public List<FriendInfoInterface> getAllFriendInfo(String myNickname) {
+        return friendRepository.findAllFriendByNickname(myNickname);
+    }
 
 
-	@Override
-	/**
-	 * 친구관계 추가하는 메서드
-	 */
-	public Friend createFriend(String myNickname, String friendNickname) {
-		Friend friend = new Friend();
-		friend.setMyNickname(myNickname);
-		friend.setFriendNickname(friendNickname);
-		return friendRepository.save(friend);
-	}
+    @Override
+    /**
+     * 친구관계 추가하는 메서드
+     */
+    public Friend createFriend(String myNickname, String friendNickname) {
+        Friend friend = new Friend();
+        friend.setMyNickname(myNickname);
+        friend.setFriendNickname(friendNickname);
+        return friendRepository.save(friend);
+    }
 
-	
-	@Override
-	/**
-	 * 친구관계가 존재하는지 확인하는 메서드
-	 */
-	public boolean alreadyFriend(String myNickname, String friendNickname) {
-		if(friendRepository.findByMyNicknameAndFriendNickname(myNickname, friendNickname) == null){
-			return false;
-		} else {
-			return true;
-		}
-	}
+
+    @Override
+    /**
+     * 친구관계가 존재하는지 확인하는 메서드
+     */
+    public boolean alreadyFriend(String myNickname, String friendNickname) {
+        if (friendRepository.findByMyNicknameAndFriendNickname(myNickname, friendNickname) == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * 친구 삭제 메서드
+     */
+    @Override
+    @Transactional
+    public boolean deleteFriend(String myNickname, String friendNickname) {
+        Friend friend = friendRepository.findByMyNicknameAndFriendNickname(myNickname, friendNickname);
+        if (friend == null) {
+            return false;
+        }
+        try {
+            friendRepository.deleteById(friend.getFriendSeq());
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
 
 }
